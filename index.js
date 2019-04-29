e6a975201914ce402957749a7a2f02489d06ff90
Clement Lakhal <clementlakhal@gmail.com>
2019-04-29 11:11:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const Gun = require('gun');
+const express = require('express');
+
+const app = express();
+app.use(Gun.serve);
+app.use(express.static(__dirname));
+
+const server = app.listen(3000);
+const gun = Gun({
+  file: 'data',
+  web: server,
+});
